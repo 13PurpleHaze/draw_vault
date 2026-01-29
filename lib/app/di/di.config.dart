178d204ba83fdd9c52009396cec983ce04d83405
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2026-01-29 19:22:29 +0500

    fix di

--- lib/app/di/di.config.dart
+++ lib/app/di/di.config.dart
@@ -10,7 +10,6 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:draw_vault/app/navigation/app_router.dart' as _i158;
-import 'package:draw_vault/app/navigation/guard.dart' as _i881;
 import 'package:draw_vault/app/network/connectivly_service.dart' as _i912;
 import 'package:draw_vault/app/notifications/notification_service.dart'
     as _i670;
@@ -41,6 +40,7 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.singleton<_i158.AppRouter>(() => _i158.AppRouter());
     gh.singleton<_i912.ConnectivityService>(() => _i912.ConnectivityService());
     gh.singleton<_i670.NotificationService>(() => _i670.NotificationService());
     gh.singleton<_i1054.FirebasePictureRepository>(
@@ -71,13 +71,6 @@ extension GetItInjectableX on _i174.GetIt {
         connectivityService: gh<_i912.ConnectivityService>(),
       ),
     );
-    gh.singleton<_i881.AuthGuard>(
-      () => _i881.AuthGuard(
-        authRepo: gh<_i1055.AuthRepository>(),
-        authBloc: gh<_i751.AuthBloc>(),
-      ),
-    );
-    gh.singleton<_i158.AppRouter>(() => _i158.AppRouter(gh<_i881.AuthGuard>()));
     return this;
   }
 }
